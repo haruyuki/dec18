7597e88900872c2b82c7979f704ac96f6e24123f
Oliver Lin <jumpy12359@gmail.com>
2024-12-05 01:39:39 +1100

    🔥 Remove unneccessary files

--- src/lib/types.ts
+++
@@ -1,6 +0,0 @@
-export interface Pet {
-  petID: string;
-  petYear: string;
-  petEvent: string;
-  petLink: string;
-}
--- src/lib/utils/imageUtils.ts
+++
@@ -1,9 +0,0 @@
-export function updateImageBackground(imageUrl: string, bgColor: string): string {
-  try {
-    const url = new URL(imageUrl);
-    url.searchParams.set('bg', bgColor.replace('#', ''));
-    return url.toString();
-  } catch {
-    return imageUrl;
-  }
-}
